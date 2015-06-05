e5565de40f72fed47a72e8e9d1451ce43c9b1e9a
Eliel de Paula <elieldepaula@gmail.com>
2015-06-05 20:54:35 -0300

    Envio do Readme e Banco de Dados.

---
+++ database.sql
@@ -0,0 +1,131 @@
+
+--
+-- Banco de dados: `project_manager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `files`
+--
+
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `projeto_id` int(11) NOT NULL,
+  `descricao` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `file` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_files_projetos1_idx` (`projeto_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `mensagens`
+--
+
+CREATE TABLE IF NOT EXISTS `mensagens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tarefa_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `mensagem` text COLLATE utf8_bin,
+  `data` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_mensagens_usuarios1_idx` (`usuario_id`),
+  KEY `fk_mensagens_tarefas1_idx` (`tarefa_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `perfil`
+--
+
+CREATE TABLE IF NOT EXISTS `perfil` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `avatar` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `telefone` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `facebook` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `twitter` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `github` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_perfil_usuarios1_idx` (`usuario_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `projetos`
+--
+
+CREATE TABLE IF NOT EXISTS `projetos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(11) NOT NULL,
+  `titulo` varchar(200) CHARACTER SET latin1 NOT NULL,
+  `descricao` text CHARACTER SET latin1 NOT NULL,
+  `inincio` date NOT NULL,
+  `fim` date NOT NULL,
+  `status` varchar(15) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_projetos_usuarios_idx` (`usuario_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Fazendo dump de dados para tabela `projetos`
+--
+
+INSERT INTO `projetos` (`id`, `usuario_id`, `titulo`, `descricao`, `inincio`, `fim`, `status`) VALUES
+(1, 0, 'Projeto de exemplo', 'Descrição do projeto de exemplo.', '2015-06-01', '2015-06-10', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tarefas`
+--
+
+CREATE TABLE IF NOT EXISTS `tarefas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `projeto_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `descricao` text CHARACTER SET latin1 NOT NULL,
+  `inicio` date NOT NULL,
+  `fim` date DEFAULT NULL,
+  `status` varchar(25) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_tarefas_usuarios1_idx` (`usuario_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Fazendo dump de dados para tabela `tarefas`
+--
+
+INSERT INTO `tarefas` (`id`, `projeto_id`, `usuario_id`, `descricao`, `inicio`, `fim`, `status`) VALUES
+(1, 1, 1, 'Descrição de uma tarefa de exemplo.', '2015-06-06', '2015-06-10', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(120) CHARACTER SET latin1 NOT NULL,
+  `funcao` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `email` varchar(120) CHARACTER SET latin1 NOT NULL,
+  `senha` varchar(120) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Fazendo dump de dados para tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `funcao`, `email`, `senha`) VALUES
+(1, 'Administrador', 'Gerente', 'admin@admin', '21232f297a57a5a743894a0e4a801fc3');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,47 @@
+#PHP - Project Manager
+
+O PHP Project Manager é um gerenciador de projetos simples com foco no controle das tarefas. Eu o desenvolvi visando controlar o meu fluxo de tarefas de vários projetos.
+
+Apesar de já existirem muitos e bons gerenciadores de projetos disponíveis na internet, resolvi desenvolver o meu próprio justamente para enxugar e manter somente as ferramentas e recursos que são realmente uteis para mim.
+
+Sinta-se a vontade para usar, testar, comentar e codificar também.
+
+Foi desenvolvido usando PHP, Codeigniter e MySQL.
+
+###Requisitos
+
+- Servidor com PHP 5 e MySql.
+
+###Instalação
+
+- Faça o download ou clone este repositório;
+- Carregue todos os arquivos para o servidor onde ficará hospedado ou em seu servidor local;
+- Crie uma base de dados e carregue o database.sql incluíudo, em seguida configure os dados da conexão no arquivo '/app/config/database.php';
+- Acesse o local de instalação pelo navegador, você deverá ver uma tela de login;
+- Use os dados para o primeiro login:
+
+  Email: admin@admin
+  Senha: admin
+
+#Licença
+
+Este é um projeto para uso pessoal, estou disponibilizando sob a licença MIT, você pode usar da forma que achar melhor, mas sem nenhuma garantia. Se quiser contribuir para o projeto, entre em contato comigo pelo email dev[at]elieldepaula.com.br, clone o repositório e vamos trabalhar ;)
+
+#The MIT License (MIT)
+
+Copyright (c) 2014 Eliel de Paula
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
